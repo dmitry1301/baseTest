29731253041f8bc9d5da906cfabffa95aa1b716c
dmitry1301 <dmitry.sergeevrap@gmail.com>
2021-11-19 20:13:13 +0500

    add db model

---
+++ baseTest/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using baseTest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace baseTest.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> users { get; set; }
+        public DbSet<Ticket> tickets { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ baseTest/Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace baseTest.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Description { get; set; }
+        public string NameProject { get; set; }
+        public DateTime CloseDate { get; set; }
+        public string TicketStatus { get; set; }
+
+        public int UserId { get; set; }  //ссылка на модель User
+        public User User { get; set; }
+    }
+}
